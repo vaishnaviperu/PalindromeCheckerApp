7913ef88fa7b0311f99df78b29ec495310963320
vaishnaviperu <vaishnaviperu07@gmail.com>
2026-02-27 16:18:33 +0530

    UC4 palindrome using character array two pointer

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,33 +1,33 @@
-public class PalindromeCheckerApp {
-    /**
-     * ============================================================
-     * MAIN CLASS - UseCase1PalindromeApp
-     * ============================================================
-     *
-     * Use Case 1: Application Entry & Welcome Message
-     *
-     * Description:
-     * This class represents the entry point of the
-     * Palindrome Checker Management System.
-     *
-     * At this stage, the application:
-     * - Starts execution from the main() method
-     * - Displays a welcome message
-     * - Shows application version
-     *
-     * No palindrome logic is implemented yet.
-     *
-     * The goal is to establish a clear startup flow.
-     *
-     * @author Devolper
-     * @version 1.0
-     */
-
-
+import java.util.Scanner;
+public class UseCase4PalindromeCheckerApp {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully.");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter text: ");
+        String input = scanner.nextLine();
+
+        input = input.toLowerCase();
+
+        char[] chars = input.toCharArray();
+
+        int start = 0;
+        int end = chars.length - 1;
+
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
+
+        scanner.close();
     }
 }
