062284549aef9bbbba3bf7134b5f50946ed026be
vaishnaviperu <vaishnaviperu07@gmail.com>
2026-02-27 16:24:09 +0530

    UC5 palindrome using stack

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,32 +1,23 @@
 import java.util.Scanner;
-public class UseCase4PalindromeCheckerApp {
-
+import java.util.Stack;
+public class UseCase5PalindromeCheckerApp {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Enter text: ");
         String input = scanner.nextLine();
-
         input = input.toLowerCase();
-
-        char[] chars = input.toCharArray();
-
-        int start = 0;
-        int end = chars.length - 1;
-
+        Stack<Character> stack = new Stack<>();
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
         boolean isPalindrome = true;
-
-        while (start < end) {
-            if (chars[start] != chars[end]) {
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
                 isPalindrome = false;
                 break;
             }
-            start++;
-            end--;
         }
-
-        System.out.println("Is it a Palindrome? : " + isPalindrome);
+        System.out.println("Is Palindrome? : " + isPalindrome);
 
         scanner.close();
     }
