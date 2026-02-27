ec3b7ddffc8cb6ca58254da8c1448a8b5e8100d4
vaishnaviperu <vaishnaviperu07@gmail.com>
2026-02-27 15:59:49 +0530

    UC3 palindrome using string reverse

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,33 +1,28 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
-    /**
-     * ============================================================
-     * MAIN CLASS - UseCase1PalindromeApp
-     * ============================================================
-     *
-     * Use Case 1: Application Entry & Welcome Message
-     *
-     * Description:
-     * This class represents the entry point of the
-     * Palindrome Checker Management System.
-     *
-     * At this stage, the application:
-     * - Starts execution from the main() method
-     * - Displays a welcome message
-     * - Shows application version
-     *
-     * No palindrome logic is implemented yet.
-     *
-     * The goal is to establish a clear startup flow.
-     *
-     * @author Devolper
-     * @version 1.0
-     */
-
-
 
     public static void main(String[] args) {
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully.");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter text: ");
+        String input = scanner.nextLine();
+
+        String reversed = "";
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
+        }
+
+        System.out.println("Reversed text: " + reversed);
+
+        if (input.equals(reversed)) {
+            System.out.println("Is it a Palindrome? : true");
+        } else {
+            System.out.println("Is it a Palindrome? : false");
+        }
+
+        scanner.close();
     }
 }
